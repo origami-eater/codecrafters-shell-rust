f00618f8252f5c4332f65ffa96f34ac3681c89ab
PSNPT <90635713+PSNPT@users.noreply.github.com>
2025-11-05 18:27:04 -0500

    error on any command

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    print!("{}: command not found", input);
+
     io::stdout().flush().unwrap();
 }
