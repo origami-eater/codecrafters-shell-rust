b2f08120a5588129c4ad2286ed99efc72644047d
PSNPT <90635713+PSNPT@users.noreply.github.com>
2025-11-09 18:28:20 -0500

    probably overcomplicated implementation for handling commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,49 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{collections::HashMap, process::exit};
+
+struct Command {
+    id: String,
+    args: Vec<String>,
+}
+
+impl Command {
+    fn parse(input: String) -> Option<Command> {
+        let tokens: Vec<String> = input
+            .trim()
+            .split(' ')
+            .map(|token| token.trim().to_owned())
+            .filter(|token| !token.is_empty())
+            .collect();
+
+        match tokens.as_slice() {
+            [id, args @ ..] => Some(Command {
+                id: id.clone(),
+                args: args.to_vec(),
+            }),
+            _ => None,
+        }
+    }
+}
+
+type CommandHandler = fn(&Command) -> Result<Option<String>, Box<dyn std::error::Error>>;
+
+fn get_handlers() -> HashMap<String, CommandHandler> {
+    let mut executor_map: HashMap<String, CommandHandler> = HashMap::new();
+
+    executor_map.insert(String::from("exit"), |command| {
+        if let Some(exit_code) = command.args.first().and_then(|x| x.parse().ok()) {
+            exit(exit_code);
+        } else {
+            Ok(None)
+        }
+    });
+    executor_map
+}
 
 fn main() {
+    let handlers = get_handlers();
+    
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -11,10 +53,16 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        let sanitized_input = input.trim();
-
-        if !sanitized_input.is_empty() {
-            println!("{}: command not found", sanitized_input);
+        if let Some(command) = Command::parse(input) {
+            if let Some(handler) = handlers.get(&command.id) {
+                match handler(&command) {
+                    Ok(Some(output)) => println!("{}", output),
+                    Ok(None) => continue,
+                    Err(error) => eprintln!("Error: {}", error)
+                }
+            } else {
+                println!("{}: command not found", command.id);
+            }
         }
     }
 }
