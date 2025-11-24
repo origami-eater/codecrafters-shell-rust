6a0e2d366a0d099b8bb7b31836ed36cd1dd16a46
origami-eater <prabuddha.senapati@outlook.com>
2025-11-23 19:24:58 -0500

    refactor impl

--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,145 @@
 use std::env;
-use std::fmt;
 use std::fs;
-use std::fs::Permissions;
-#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
-use std::{collections::HashMap, process::exit};
+use std::process::exit;
+
+#[derive(Debug)]
+enum ShellError {
+    InvalidArgs(&'static str),
+}
+
+impl std::fmt::Display for ShellError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ShellError::InvalidArgs(msg) => write!(f, "{}", msg),
+        }
+    }
+}
+
+impl std::error::Error for ShellError {}
+
+type ShellResult = Result<String, Box<dyn std::error::Error>>;
 
 struct Command {
-    id: String,
+    name: String,
     args: Vec<String>,
 }
 
 impl Command {
-    fn parse(input: String) -> Option<Command> {
-        let tokens: Vec<String> = input
-            .trim()
-            .split(' ')
-            .map(|token| token.trim().to_owned())
-            .filter(|token| !token.is_empty())
-            .collect();
+    fn parse(input: &str) -> Option<Command> {
+        let tokens: Vec<&str> = input.trim().split_whitespace().collect();
 
-        match tokens.as_slice() {
-            [id, args @ ..] => Some(Command {
-                id: id.clone(),
-                args: args.to_vec(),
-            }),
+        if let Some((name, args)) = tokens.split_first() {
+            return Some(Command {
+                name: name.to_string(),
+                args: args.iter().map(|x| x.to_string()).collect(),
+            });
+        } else {
+            return None;
+        }
+    }
+}
+
+enum Builtin {
+    Exit,
+    Echo,
+    Type,
+}
+
+// manually defining it since ideally the least external dependencies
+impl Builtin {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Builtin::Echo => "echo",
+            Builtin::Exit => "exit",
+            Builtin::Type => "type",
+        }
+    }
+    fn from_str(s: &str) -> Option<Builtin> {
+        match s {
+            "echo" => Some(Builtin::Echo),
+            "exit" => Some(Builtin::Exit),
+            "type" => Some(Builtin::Type),
             _ => None,
         }
     }
 }
 
-struct Executable {
-    path: String,
-    permissions: Permissions,
-}
-
-#[derive(Debug)]
-enum ShellError {
-    CommandNotFound(String),
-}
-
-impl fmt::Display for ShellError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ShellError::CommandNotFound(cmd) => write!(f, "{}: command not found", cmd),
-        }
-    }
-}
-
-type CommandHandler = fn(&Command) -> ();
-
-fn get_handlers() -> HashMap<String, CommandHandler> {
-    let mut executor_map: HashMap<String, CommandHandler> = HashMap::new();
-
-    executor_map.insert(String::from("exit"), |command| {
-        if let Some(exit_code) = command.args.first().and_then(|x| x.parse().ok()) {
-            exit(exit_code);
-        } else {
-            exit(0);
-        }
-    });
-
-    executor_map.insert(String::from("echo"), |command| {
-        println!("{}", command.args.join(" "));
-    });
-
-    executor_map
-}
-
-fn get_executable(name: &str) -> Option<Executable> {
-    if let Some(paths) = env::var_os("PATH") {
-        for dir in env::split_paths(&paths) {
-            let abs_path = match Path::new(&dir).join(name).canonicalize() {
-                Ok(abs_path) => abs_path,
-                _ => continue,
-            };
-            let metadata = fs::metadata(&abs_path).ok()?;
-            let perms = metadata.permissions();
-            if perms.mode() & 0o111 != 0 {
-                return abs_path.to_str().map(|x| Executable {
-                    path: x.to_owned(),
-                    permissions: perms,
-                });
+fn run_builtin(b: &Builtin, args: &[String]) -> ShellResult {
+    return match b {
+        Builtin::Echo => Ok(args.join(" ")),
+        Builtin::Exit => {
+            if let Some(exit_code) = args.first().and_then(|x| x.parse().ok()) {
+                exit(exit_code);
             } else {
-                continue;
+                exit(0);
             }
         }
-    }
-    return None;
+        Builtin::Type => {
+            let query = args
+                .first()
+                .ok_or(ShellError::InvalidArgs("type requires an argument"))?;
+
+            return if let Some(_) = Builtin::from_str(query) {
+                Ok(format!("{} is a shell builtin", query))
+            } else if let Some(sys_exec) = SystemExecutable::from_str(&query) {
+                Ok(format!("{} is {}", query, sys_exec.abs_path))
+            } else {
+                Ok(format!("{}: not found", query))
+            };
+        }
+    };
 }
 
-fn run_process(command: &Command, executable: &Executable) {
-    let process = match std::process::Command::new(&command.id)
-        .args(command.args.clone())
-        .spawn()
-    {
-        Ok(process) => process,
-        Err(err) => panic!("encountered error spawning a process: {}", err),
-    };
+struct SystemExecutable {
+    name: String,
+    abs_path: String,
+}
 
-    let output = match process.wait_with_output() {
-        Ok(output) => output,
-        Err(err) => panic!("Retrieving output error: {}", err),
-    };
+impl SystemExecutable {
+    fn from_str(exec_name: &str) -> Option<SystemExecutable> {
+        let paths = env::var_os("PATH")?;
 
-    let stdout = match std::string::String::from_utf8(output.stdout) {
-        Ok(stdout) => stdout,
-        Err(err) => panic!("Translating output error: {}", err),
-    };
+        return env::split_paths(&paths).find_map(|dir| {
+            let abs_path = Path::new(&dir).join(exec_name).canonicalize().ok()?;
+            let perms = fs::metadata(&abs_path).ok()?.permissions();
 
-    print!("{}", stdout);
+            if perms.mode() & 0o111 != 0 {
+                return abs_path.to_str().map(|x| SystemExecutable {
+                    name: exec_name.to_owned(),
+                    abs_path: x.to_owned(),
+                });
+            } else {
+                None
+            }
+        });
+    }
+    fn run_foreground(&self, args: &[String]) -> ShellResult {
+        let process = std::process::Command::new(&self.name)
+            .args(args.clone())
+            .spawn()?;
+
+        let output = process.wait_with_output()?;
+
+        let stdout = std::string::String::from_utf8(output.stdout)?;
+
+        Ok(stdout)
+    }
+}
+
+fn process_command(cmd: &Command) -> ShellResult {
+    return if let Some(builtin) = Builtin::from_str(&cmd.name) {
+        run_builtin(&builtin, &cmd.args)
+    } else if let Some(sys_exec) = SystemExecutable::from_str(&cmd.name) {
+        sys_exec.run_foreground(&cmd.args)
+    } else {
+        Ok(format!("{}: command not found", &cmd.name))
+    };
 }
 
 fn main() {
-    let handlers = get_handlers();
-
-    let run_command_or_error = |command: &Command| -> Option<ShellError> {
-        match &command.id {
-            id if id == "type" => {
-                if let Some(query) = command.args.first() {
-                    if handlers.contains_key(query) || query == "type" {
-                        println!("{} is a shell builtin", query);
-                    } else if let Some(executable) = get_executable(&query) {
-                        println!("{} is {}", query, executable.path);
-                    } else {
-                        println!("{}: not found", query);
-                    }
-                    return None;
-                }
-            }
-            _ => {}
-        }
-
-        if let Some(handler) = handlers.get(&command.id) {
-            handler(&command);
-            return None;
-        } else if let Some(executable) = get_executable(&command.id) {
-            run_process(command, &executable);
-            return None;
-        } else {
-            return Some(ShellError::CommandNotFound(command.id.clone()))
-        }
-    };
-
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -154,10 +149,10 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        if let Some(command) = Command::parse(input) {
-            match run_command_or_error(&command) {
-                Some(error) => eprintln!("{}", error),
-                None => {}
+        if let Some(cmd) = Command::parse(&input) {
+            match process_command(&cmd) {
+                Ok(out) => println!("{}", out),
+                Err(err) => println!("{}", err),
             }
         }
     }
